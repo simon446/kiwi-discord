9b015acc66976c51089642057b011f99928be372
froadus <simjoh99@gmail.com>
2019-10-18 21:41:55 +0200

    Add database module for private settings

---
+++ app/modules/discord-db.js
@@ -0,0 +1,80 @@
+/**
+ * Use a discord message as a database
+ */
+
+module.exports = class DiscordDB {
+    constructor({ DB_CHANNEL_NAME='bot_db', COMMAND='db' }) {
+        this.DB_CHANNEL_NAME = DB_CHANNEL_NAME;
+        this.COMMAND = COMMAND
+    }
+
+    register(bot) {
+        bot.onCommand(this.COMMAND, async (messageString, discordMessage) => {
+            let args = messageString.split('=');
+            args = args.map(arg => arg.trim());
+            let db = await this.getDB();
+            if (args[1] !== undefined) {
+                try {
+                    db[args[0]] = JSON.parse(args[1]);
+                } catch (err) {
+                    return discordMessage.channel.send('Invalid input')
+                }
+            } else {
+                db[args[0]] = undefined;
+            }
+            await this.writeDB(db);
+            return discordMessage.channel.send(`\`\`\`json\n{ ${args[0]}: ${db[args[0]]} }\n\`\`\``)
+        });
+
+        bot.onReadyPreload(client => {
+            this.CLIENT = client;
+        });
+    }
+
+    get DB_CHANNEL() {
+        return this.CLIENT.channels.find(channel => channel.name === this.DB_CHANNEL_NAME);
+    }
+
+    async dbMessage() {
+        let channel = this.DB_CHANNEL;
+        if (channel === null) console.log(`Channel ${this.DB_CHANNEL_NAME} not found.`)
+        else {
+            try {
+                return await channel.fetchMessage(channel.lastMessageID);
+            } catch (err) {
+                throw new Error("Cannot get last message.")
+            }
+        }
+    }
+
+    async getDB() {
+        let message;
+        try {
+            message = await this.dbMessage();
+            return JSON.parse(/```json\s+?(.*)```/ms.exec(message.content)[1]);
+        } catch (err) {
+            return {}
+        }
+    }
+
+    async writeDB(newObject) {
+        let message, msgStr;
+        try {
+            message = await this.dbMessage();
+            msgStr = '```json\n' + JSON.stringify(newObject, null, 4) + '```';
+        } catch (err) {
+            msgStr = '```json\n' + JSON.stringify(newObject, null, 4) + '```';
+            return await this.DB_CHANNEL.send(msgStr)
+        }
+        
+        if (message.author.id === this.CLIENT.user.id) { // This bot sent the message and can edit it
+            return await message.edit(msgStr)
+        } else { // Send new message that this bot can edit
+            return await this.DB_CHANNEL.send(msgStr)
+        }
+    }
+
+    get help() {
+        return [`!**${this.COMMAND} key=value** to set a value in the database.`]
+    }
+}
